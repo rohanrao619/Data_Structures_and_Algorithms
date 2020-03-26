9f6ece057130b196df4f30c9168f3765836915fd
S Rohan Rao <rohanrao619@gmail.com>
2020-03-26 23:34:35 +0530

    Add Sorting Algorithms

---
+++ Sorting/Quick_Sort_using_Hoare_Partition.cpp
@@ -0,0 +1,83 @@
+// { Driver Code Starts
+#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+int partition (int arr[], int low, int high);
+/* The main function that implements QuickSort
+ arr[] --> Array to be sorted, low  --> Starting index, high  --> Ending index */
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        /* pi is partitioning index, arr[p] is now at right place */
+        int pi = partition(arr, low, high);
+        // Separately sort elements before partition and after partition
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+// Driver program to test above functions
+int main()
+{
+    int arr[1000],n,T,i;
+    scanf("%d",&T);
+    while(T--){
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+    quickSort(arr, 0, n-1);
+    printArray(arr, n);
+    }
+    return 0;
+}// } Driver Code Ends
+/* The main function that implements QuickSort
+ arr[] --> Array to be sorted,    low  --> Starting index,   high  --> Ending index
+void quickSort(int arr[], int low, int high) {
+    if (low < high)     {
+        // pi is partitioning index, arr[p] is now  at right place
+        int pi = partition(arr, low, high);
+        // Separately sort elements before / partition and after partition
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}*/
+/* This function takes last element as pivot, places  the pivot element 
+   at its correct position in sorted  array, and places all smaller (smaller
+   than pivot) to left of pivot and all greater elements to right  of pivot */
+int partition (int arr[], int low, int high)
+{
+   // Your code here
+   int i=low+1,j=high,key=arr[low],temp;
+   
+   while(true)
+   {
+       while(arr[i]<key && i<high)
+       i++;
+       while(arr[j]>=key && j>low)
+       j--;
+       
+       if(i<j)
+       {
+           temp=arr[i];
+           arr[i]=arr[j];
+           arr[j]=temp;
+       }
+       else
+       break;
+   }
+   
+   temp=arr[low];
+   arr[low]=arr[j];
+   arr[j]=temp;
+   
+   return j;
+}
+
